df01fa3ef53f0ea02b6edfcb815dd67fa7f3b326
Hayden Labrie <haydenlabriecs@gmail.com>
2022-10-09 18:17:19 -0500

    Changes by jovanniochoa

--- main.py
+++ main.py
@@ -23,8 +23,22 @@ mycursor = mydb.cursor()
 mycursor.execute("SELECT first_name, last_name, studentlogin FROM student WHERE first_name = %s", (firstName,))
 
 x = mycursor.fetchall()
-print(x[0][0])
+print(x)
+i = 0
+while x[i][1] != lastName:
+    i = i + 1
 
+##Prints user id
+currentUserID = x[i][2]
+print(currentUserID)
+
+#gets the appid, appdetails, start, end, totalSpentTime, urlAppId, and sort by time spent
+mycursor.execute("SELECT appid, appdetails, start, end, totalSpentTime, urlAppId FROM swye360.students_analytics WHERE loginid= %s ORDER BY totalSpentTime DESC", (currentUserID,))
+x = mycursor.fetchall()
+print(x)
+
+
+#next step is getting rid of duplicate urlAppId's since you can't do it in SQL. Get top 5 unique...
 
 # mycursor = mydb.cursor()
 #
