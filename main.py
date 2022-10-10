1ad2c859e23f41967d8dc81d4c8fa2510be17656
Hayden Labrie <haydenlabriecs@gmail.com>
2022-10-10 17:36:30 -0500

    added pie chart

--- main.py
+++ main.py
@@ -19,8 +19,8 @@ mydb = mysql.connector.connect(
 
 # firstName = input("Please enter First name: ")
 # lastName = input("Please enter Last name: ")
-firstName = "Hayden"
-lastName = "Jones"
+firstName = "Kyle"
+lastName = "Beeson"
 
 mycursor = mydb.cursor()
 mycursor.execute("SELECT first_name, last_name, studentlogin FROM student WHERE first_name = %s AND last_name = %s",
@@ -32,23 +32,17 @@ x = mycursor.fetchall()
 currentUserID = x[0][2]
 # print(currentUserID)
 
-# gets the appid, appdetails, start, end, totalSpentTime, urlAppId, and sort by time spent
-mycursor.execute(
-    "SELECT appid, appdetails, start, end, totalSpentTime, urlAppId FROM swye360.students_analytics WHERE loginid= %s ORDER BY totalSpentTime DESC",
-    (currentUserID,))
-x = mycursor.fetchall()
-# print(x[0][5])
-
 # Gets rid of duplicate urlAppId's SQL command that groups by appid and sums up the total time spent for duplicate
 # appid values. Returns appid, the app information, start and end time, sum total, and URLappID.
 mycursor.execute(
-    "SELECT appid, MAX(appdetails), MAX(start), MAX(end), SUM(totalSpentTime), MAX(urlAppId) FROM swye360.students_analytics group by appid ORDER BY SUM(totalSpentTime) DESC")
+    "SELECT urlAppId, appdetails, SUM(totalSpentTime) AS TotalTime FROM students_analytics WHERE loginid=%s GROUP BY urlAppId ORDER BY TotalTime DESC",
+    (currentUserID,))
 top_used_software = mycursor.fetchall()
-print("#1: " + top_used_software[0][1] + " ---- " + "Total time: " + str(top_used_software[0][4]))
-print("#2: " + top_used_software[1][1] + " ---- " + "Total time: " + str(top_used_software[1][4]))
-print("#3: " + top_used_software[2][1] + " ---- " + "Total time: " + str(top_used_software[2][4]))
-print("#4: " + top_used_software[3][1] + " ---- " + "Total time: " + str(top_used_software[3][4]))
-print("#5: " + top_used_software[4][1] + " ---- " + "Total time: " + str(top_used_software[4][4]))
+print("#1: " + top_used_software[0][1] + " ---- " + "Total time: " + str(top_used_software[0][2]))
+print("#2: " + top_used_software[1][1] + " ---- " + "Total time: " + str(top_used_software[1][2]))
+print("#3: " + top_used_software[2][1] + " ---- " + "Total time: " + str(top_used_software[2][2]))
+print("#4: " + top_used_software[3][1] + " ---- " + "Total time: " + str(top_used_software[3][2]))
+print("#5: " + top_used_software[4][1] + " ---- " + "Total time: " + str(top_used_software[4][2]))
 
 # parses each website via objects
 W1 = urlparse(top_used_software[0][1])
@@ -57,18 +51,18 @@ W3 = urlparse(top_used_software[2][1])
 W4 = urlparse(top_used_software[3][1])
 W5 = urlparse(top_used_software[4][1])
 
-sizes = [int(top_used_software[0][4]),
-        int(top_used_software[1][4]),
-        int(top_used_software[2][4]),
-        int(top_used_software[3][4]),
-        int(top_used_software[4][4])]
+sizes = [int(top_used_software[0][2]),
+        int(top_used_software[1][2]),
+        int(top_used_software[2][2]),
+        int(top_used_software[3][2]),
+        int(top_used_software[4][2])]
 Website1 = W1.netloc
 Website2 = W2.netloc
 Website3 = W3.netloc
 Website4 = W4.netloc
 Website5 = W5.netloc
 
-if Website1 == '':
+if W1.netloc == '':
     Website1 = W1.path
 if Website2 == '':
     Website2 = W2.path
@@ -85,7 +79,8 @@ labels = [str(Website1), str(Website2), str(Website3), str(Website4), str(Websit
 fig, ax = plt.subplots()
 ax.pie(sizes, labels=labels, autopct='%1.1f%%')
 ax.axis('equal')  # Equal aspect ratio ensures the pie chart is circular.
-ax.set_title('Top Used Software')
+ax.set_title('TOP 5 USED SOFTWARE FOR\n' + firstName + " - " + lastName)
 
 
 plt.show()
+
